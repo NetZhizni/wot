edcb9aa9cb5d298c5cb5ea808dc5c414649d76f1
Oleksiy Leleka <oleksiyleleka@gmail.com>
2025-09-24 03:35:53 +0300

    fix: v7

--- custom_components/wot/config_flow.py
+++ custom_components/wot/config_flow.py
@@ -1,12 +1,6 @@
-import logging
 import voluptuous as vol
-
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
-
 from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
 
 DATA_SCHEMA = vol.Schema({
     vol.Required("application_id"): str,
@@ -14,17 +8,13 @@ DATA_SCHEMA = vol.Schema({
 })
 
 class WOTConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for WOT integration."""
+    """Handle WOT config flow."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            # Можна додати перевірку токену через API
             return self.async_create_entry(title="WOT Reserves", data=user_input)
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=DATA_SCHEMA
-        )
+        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
