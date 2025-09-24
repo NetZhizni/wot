6a96a3ac562ff2c9b57890763e0965c3670f48ed
Oleksiy Leleka <oleksiyleleka@gmail.com>
2025-09-24 03:44:18 +0300

    fix: v8

--- custom_components/wot/__init__.py
+++ custom_components/wot/__init__.py
@@ -3,5 +3,12 @@ from homeassistant.core import HomeAssistant
 DOMAIN = "wot"
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the WOT integration (no YAML)."""
+    """Set up the integration (for YAML, якщо буде, але зараз можна залишити True)."""
+    return True
+
+async def async_setup_entry(hass, entry):
+    """Set up WOT integration from a config entry."""
+    # Зберігаємо дані конфігурації, щоб сенсори могли до них звертатись
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
     return True
